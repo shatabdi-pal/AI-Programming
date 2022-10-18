e1c690279dc7adb0f67ac6eef5f15022f467ab43
shatabdi-pal <shatabdi@pdx.edu>
2022-10-18 12:06:15 -0700

    A*search algorithm

--- Asearch.py
+++ Asearch.py
@@ -1,19 +1,58 @@
-#Taking user input for initial state
-#0 is considered as blank
-initial_state = []
-print("Please input values from 0-8 for the initial state")
-for i in range(0,9):
-    x = int(input("enter values:"))
-    initial_state.append(x)
+import numpy as np
 
-print(initial_state)
+# Taking user input for initial state
+# b is considered as blank
+# initial_state = []
+# print("Please input values for the initial state")
+# for i in range(0,9):
+#     x = (input("enter values:"))
+#     initial_state.append(x)
+#
+# initial = tuple(initial_state)
+
+initial = ("b", "2", "3", "1", "4", "5", "8", "7", "6")
+print(initial)
 
 #Taking user input for goal state
-#0 is considered as blank
-goal_state = []
-print("Please input values from 0-8 for the goal state")
-for j in range(0,9):
-    y = int(input("enter values:"))
-    goal_state.append(y)
-print(goal_state)
+#b is considered as blank
+# goal_state = []
+# print("Please input values from 0-8 for the goal state")
+# for j in range(0,9):
+#     y = (input("enter values:"))
+#     goal_state.append(y)
+
+# goal = tuple(goal_state)
+
+goal = ("1","2","3","8", "b", "4", "7", "6", "5")
+print(goal)
+
+print("Please enter your choice to run A* search algorithm: \n")
+choice = int(input("1.Misplaced tiles  \n2. Manhattan distance \n3. Euclidean Distance"))
+
+def manhattan_distance(initial, goal):
+    distance = 0
+    for x_i, y_i in zip(initial,goal):
+        if x_i != 'b' and y_i !='b':
+            distance += abs(int(x_i) - int(y_i))
+    return distance
+
+
+def misplaced_tiles(initial, goal):
+    if (initial != goal):
+        hcost = np.sum(initial != goal) - 1
+    else:
+        hcost = 0
+    return hcost
+
+
+#Huristic#1
+hcost_manhattan = manhattan_distance(initial, goal)
+print("huristic cost for manhattan distance: ", hcost_manhattan)
+
+#Huristic#2
+hcost = misplaced_tiles(initial, goal)
+print("huristic cost for misplaced tiles: ", hcost)
+
+#Huristic#3
+
 
